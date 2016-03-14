f688b36ac7022c551d1a0ddf4082c272af97bf90
webdude21 <dimo.petrof@gmail.com>
2016-03-14 23:13:01 +0200

    Added two more exercises

---
+++ src/corejava/fundamentals/BigFactorial.java
@@ -0,0 +1,23 @@
+package corejava.fundamentals;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigFactorial {
+
+
+    private static BigInteger one = BigInteger.valueOf(1);
+
+    public static BigInteger calcFactorial(BigInteger input) {
+        if (input.equals(one)) {
+            return one;
+        }
+
+        return input.multiply(calcFactorial(input.subtract(one)));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(calcFactorial(scanner.nextBigInteger()));
+    }
+}
